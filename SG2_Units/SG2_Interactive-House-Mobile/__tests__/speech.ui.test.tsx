75bd34ad6fdde12d33f8695aadc2ec00e9972cb7
Daniel Jönsson <daniel.jonsson0131@stud.hkr.se>
2026-03-15 18:18:00 +0100

    SG2: Remove not working tests

--- SG2_Units/SG2_Interactive-House-Mobile/__tests__/speech.ui.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
-import SpeechScreen from '../app/(tabs)/speech';
-
-test('SpeechScreen shows placeholder text', () => {
-  const { getByText } = render(<SpeechScreen />);
-  getByText('Voice Control');
-  getByText('Your speech will appear here...');
-});
